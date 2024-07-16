ee214833607f609a43bcf72274c68b1a56e5dccc
Mfreitas0 <mfreitasc0@gmail.com>
2024-07-16 06:51:02 -0300

    Refatorando estrutura do projeto: Criado routers para Users e Auth; movido constantes para variáveis de ambiente.

---
+++ tests/test_users.py
@@ -0,0 +1,131 @@
+from http import HTTPStatus
+
+from fast_zero.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
+
+
+def test_create_user_username_exists(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice2@example.com",
+            "password": "secret",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Username already exists"}
+
+
+def test_create_user_email_exists(client):
+    response_email = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+    response_email = client.post(
+        "/users/",
+        json={
+            "username": "keles",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
+
+    assert response_email.status_code == HTTPStatus.BAD_REQUEST
+    assert response_email.json() == {"detail": "Email already exists"}
+
+
+def test_read_users(client):
+    response = client.get(
+        "/users/",
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"users": []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get("/users/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"users": [user_schema]}
+
+
+def test_update_user(client, user, token):
+    response = client.put(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": 1,
+    }
+
+
+def test_update_wrong_user(client, user, token):
+    response = client.put(
+        f"/users/{user.id + 1}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {"detail": "Not enough permission"}
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f"/users/{user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "User deleted"}
+
+
+def test_delete_wrong_user(client, user, token):
+    response = client.delete(
+        f"/users/{user.id + 1}",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {"detail": "Not enough permission"}
